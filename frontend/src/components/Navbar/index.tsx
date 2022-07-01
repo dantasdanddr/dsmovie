aa39eff44795a922f4d8b7b6673821f1ad5ab99e
Daniel Dantas <dantas.dan.ddr@gmail.com>
2022-06-30 22:50:28 -0300

    useParams

--- frontend/src/components/Navbar/index.tsx
+++ frontend/src/components/Navbar/index.tsx
@@ -7,7 +7,7 @@ function Navbar(){
             <nav className="container">
                 <div className="dsmovie-nav-content">
                     <h1>DSMovie</h1>
-                    <a href="https://github.com/dantasdanddr" target="_blank" rel="noreferrer">
+                    <a href="https://dantasdanddr.github.io/" target="_blank" rel="noreferrer">
                         <div className="dsmovie-contact-container">
                             <GithubIcon />
                             <p className="dsmovie-contact-link">/dantasdanddr</p>
